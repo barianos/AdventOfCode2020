8566047cdfd05d4eea4c6ed806afa6ca8475b3dc
Anastasios Barianos <barianos@outlook.com>
2020-12-04 12:31:03 +0200

    Add project files.

---
+++ Advent Of Code/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Advent_Of_Code
+{
+    class Program
+    {
+        static int CURRENT_YEAR = 2020;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********Welcome to the Advent Of Code!*******\n -------------------");
+            
+            //Day 1 Solution 2
+            FirstDay firstDay = new FirstDay();
+            ArrayList listOfAddents = firstDay.findSumPositions(CURRENT_YEAR);
+            int day1Solution = firstDay.generateArraylistProduct(listOfAddents);
+        }
+
+
+
+
+    }
+
+
+}
