cb233a8cffda611e72858a7b0484848aa3fed38b
Anastasios Barianos <barianos@outlook.com>
2020-12-04 21:05:11 +0200

    added day 2

--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -10,11 +10,17 @@ namespace Advent_Of_Code
         static void Main(string[] args)
         {
             Console.WriteLine("*********Welcome to the Advent Of Code!*******\n -------------------");
-            
+                        
+            //Day2 Solution 2
+            PasswordDebug pd = new PasswordDebug();
+            int solution = pd.analizePasswords();
+            Console.WriteLine("Solution is: " + solution);
+
+
             //Day 1 Solution 2
-            FirstDay firstDay = new FirstDay();
-            ArrayList listOfAddents = firstDay.findSumPositions(CURRENT_YEAR);
-            int day1Solution = firstDay.generateArraylistProduct(listOfAddents);
+            //FirstDay firstDay = new FirstDay();
+            //ArrayList listOfAddents = firstDay.findSumPositions(CURRENT_YEAR);
+            //int day1Solution = firstDay.generateArraylistProduct(listOfAddents);
         }
 
 
