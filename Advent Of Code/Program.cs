65370a09bc5083eb887dc505caeaa72057376250
Anastasios Barianos <barianos@outlook.com>
2020-12-05 19:40:14 +0200

    Completed Third Day

--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -10,14 +10,37 @@ namespace Advent_Of_Code
         static void Main(string[] args)
         {
             Console.WriteLine("*********Welcome to the Advent Of Code!*******\n -------------------");
-                        
-            //Day2 Solution 2
-            PasswordDebug pd = new PasswordDebug();
-            int solution = pd.analizePasswords();
-            Console.WriteLine("Solution is: " + solution);
 
 
-            //Day 1 Solution 2
+            ////Day 3 Solution 2
+            //TreeSlopesProblem tsp = new TreeSlopesProblem();
+            //int[] treesHit = new int[5];
+            //treesHit[0] = tsp.traverseMap(1, 1);
+            //treesHit[1] = tsp.traverseMap(3, 1);
+            //treesHit[2] = tsp.traverseMap(5, 1);
+            //treesHit[3] = tsp.traverseMap(7, 1);
+            //treesHit[4] = tsp.traverseMap(1, 2);
+
+            //long prod = 1;
+            //foreach (int value in treesHit)
+            //{
+            //    prod *= value;
+            //}
+
+            //Console.WriteLine("you will hit " + prod + " trees!");
+
+            //Day 3 Solution 1
+            //TreeSlopesProblem tsp = new TreeSlopesProblem();
+            //int treesHit = tsp.traverseMap(3,1);
+            //Console.WriteLine("you will hit " + treesHit + " trees!");
+
+            ////Day2 Solution 2
+            //PasswordDebug pd = new PasswordDebug();
+            //int solution = pd.analizePasswords();
+            //Console.WriteLine("Solution is: " + solution);
+
+
+            ////Day 1 Solution 2
             //FirstDay firstDay = new FirstDay();
             //ArrayList listOfAddents = firstDay.findSumPositions(CURRENT_YEAR);
             //int day1Solution = firstDay.generateArraylistProduct(listOfAddents);
